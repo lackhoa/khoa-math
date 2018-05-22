6d2666b5497ca6c55782354b8543206ed6ba614e
Dang Khoa Vo <vodangkhoa873@gmail.com>
2018-05-22 13:38:13 +0300

    Loosen up type requirements

--- khoa_math.py
+++ khoa_math.py
@@ -7,7 +7,6 @@ class MathObject():
     This class is the base for all mathematical objects, that's why it doesn't do anything
     '''
     def __init__(self, type_, text: str = ''):
-        assert(type(type_) == MathType)
         self.text = text
         self.type = type_
     def __eq__(self, other):
@@ -18,15 +17,26 @@ class MathObject():
         if isinstance(self, other.__class__):
             return self.__dict__ == other.__dict__
         return False
+        
+    def __hash__(self):
+        '''
+        We vow that MathObjects will never change once they're created
+        '''
+        return id(self)
 
     def __repr__(self):
         return self.text
 
 class MathType(Enum):
-    PL_FORMULA = auto()
-    PL_RULE_ANNOTATION = auto()
-    PL_PROOF_LINE = auto()
     PL_CONNECTION = auto()
-    PL_PROOF = auto()
-    PL_THEOREM = auto()
-    PL_TRUTH = autho()
+    '''
+    All MathObject should have a type belonging to this enum
+    But, since there are so many types, sometimes you can just use strings
+    '''
+    PL_FORMULA = 'PL Formula'
+    PL_RULE_ANNOTATION = 'PL_RULE_ANNOTATION'
+    PL_PROOF_LINE = 'PL_PROOF_LINE'
+    PL_CONNECTION = 'PL_CONNECTION'
+    PL_PROOF = 'PL_PROOF'
+    PL_THEOREM = 'PL_THEOREM'
+    PL_TRUTH = 'PL_TRUTH'
