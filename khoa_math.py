c7483c8ef6da8fa789d66541cc0bd85ab185ae0d
lackhoa <vodangkhoa873@gmail.com>
2018-06-19 13:38:04 +0300

    The most beautiful math object design so far

--- khoa_math.py
+++ khoa_math.py
@@ -1,221 +1,109 @@
 from misc import MyEnum, car, cdr, rcar, rcdr
 from kset import KSet, KConst
 
-from abc import ABC, abstractmethod
-from copy import deepcopy
-from enum import *
 from typing import *
-from anytree import NodeMixin, Resolver, ResolverError, ChildResolverError
+from pprint import pformat
 
 
-class MathObj(ABC, NodeMixin):
-    @abstractmethod
-    def __init__(self, role, name=''):
-        self.role, self.name = role, name
-
-    def __check_loop(self, node):
-        if node is not None:
-            if node is self:
-                msg = "Cannot set parent. %r cannot be parent of itself."
-                raise LoopError(msg % self)
-            if any([n is self for n in node.path]):
-                msg = "Cannot set parent. %r is parent of %r."
-                raise LoopError(msg % (self, node))
-
-    def __attach(self, parent):
-        if parent is not None:
-            self._pre_attach(parent)
-            parentchildren = parent.__children_
-            assert not any ([child is self for child in parentchildren]),\
-                    "Tree internal data is corrupt."
-            # ATOMIC START
-            parentchildren.append(self)
-            self.__parent = parent
-            # ATOMIC END
-            self._post_attach(parent)
-
-    def get_path(self, path: str):
-        r = Resolver('role')
-        return r.get(self, path)
-
-    def _recur_test(self, func: Callable[..., bool], conj = True) -> bool:
-        res = True if conj else False
-        if type(self) == Atom: return func(self)
-        else:
-            for child in self.children:
-                if conj:
-                    if not child._recur_test(func, conj): res = False; break
-                elif not conj:
-                    if child._recur_test(func, conj): res = True; break
-        return res
-
-    def is_inconsistent(self) -> bool:
-        func = lambda a: a.vals.is_empty()
-        return self._recur_test(func, False)
-
-    def is_complete(self) -> bool:
-        func = lambda a: a.vals.is_singleton()
-        return self._recur_test(func, True)
-
-    @abstractmethod
-    def clone(self):
-        """Return a deep copy of this object."""
-        pass
-
-class Atom(MathObj):
-    def __init__(self, 
-                 vals: KSet = KConst.ANY.value,
-                 val=None,
-                 role: str='',
-                 name: str=''):
-        super().__init__(role, name)
-        self.vals = KSet(frozenset({val})) if val is not None else vals
-        self.legit = False
-
-    def _pre_attach(self, parent):
-        assert(type(parent) != Atom), 'Can\'t attach to an atom!'
-
-    def __repr__(self) -> str:
-        fields = (f for f in [self.name, self.role, 'vals='+str(self.vals)] if f != '')
-        return 'A: {}'.format(', '.join(fields))
-
-    @property
-    def children(self):
-        """No children allowed!"""
-        return []
-    @children.setter
-    def children(self, value):
-        raise Exception('Atoms cannot have children')
-
-    @property
-    def val(self):
-        assert(self.vals.is_singleton()), 'Not a singleton'
-        return self.vals[0]
-
-    @val.setter
-    def val(self, value):
-        self.vals = KSet(frozenset({value}))
-
-    def clone(self) -> 'Atom':
-        res = Atom(role=self.role, vals=self.vals.clone())
-        res.legit = self.legit
-        return res
-
-    def __eq__(self, other) -> bool:
-        return (type(self) == type(other)) and (self.vals == other.vals)
-
-    def __hash__(self) -> int:
-        return hash((self.role, self.vals))
-
-
-class Mole(MathObj):
+class Mole(dict):
     def __init__(self,
-                 type_: Optional[str],
-                 cons: KSet = KConst.STR.value,
-                 con: str=None,
-                 role: str='',
-                 name: str='',
-                 parent=None,
-                 children: Iterable[Union[Atom, 'Mole']]=[]):
-        super().__init__(role, name)
-        self.type = type_
+                 type_: KSet = KConst.STR.value,
+                 cons : KSet = KConst.STR.value,
+                 con  : str  = None,
+                 dic  : dict = {})
+        """
+        Specify either `cons` (if has multiple constructors)
+        or `con`(if has one constructor)
+        """
+        kelf.type = type_
         self.cons = KSet(frozenset({con})) if con is not None else cons
-        self.parent, self.children = parent, tuple(children)
-        self.legit = False
+        self.update(dic)
 
     def __repr__(self) -> str:
-        fields = (
-            f for f in [self.name, self.role, str(self.type), 'cons=' + str(self.cons)]
-            if f != '')
-        return 'M: {}'.format(', '.join(fields))
+        return pformat(self)
 
-    def _pre_attach(self, parent: 'Mole'):
-        assert(type(parent) != Atom), 'Can\'t attach to an atom!'
+    @property
+    def cons(self):
+        return self['cons']
+
+    @cons.setter
+    def cons(self, value):
+        self['cons'] = value
+
+    @property:
+    def type(self):
+        return self['type']
+
+    @type.setter:
+    def type(self, value):
+        self['type'] = value
 
     @property
     def con(self):
-        assert(self.cons.is_singleton()), 'Not a singleton'
+        assert(self.cons.is_singleton()), 'Constructors not a singleton'
         return self.cons[0]
 
     @con.setter
     def con(self, value):
          self.cons = KSet(frozenset({value}))
 
-    @property
-    def val(self):
-        """A molecule's value is the unmodifiable blueprint of itself"""
-        assert(self.legit), 'We are not sure yet whether this molecule exists'
-        return self
+    def get_path(self, path: str):
+        if path == '': return self
+        else: return self[car(path)].get_path(cdr(path))
 
-
-    def has_path(self, role: str) -> bool:
-        try: self.get_path(role); return True
-        except ChildResolverError: return False
+    def has_path(self, path: str) -> bool:
+        try: self.get_path(path); return True
+        except KeyError: return False
 
     def pave_way(self, path: str):
-        """Keep attaching unknown nodes until `self.has_path(path)` is True"""
+        """
+        Keep attaching new molecules until `self.has_path(path)` is True
+        """
         if not self.has_path(path):
-            if not self.has_path(car(path)):
-                new_child = Mole(role=car(path), type_=None)
-            else: new_child = self.get_path(car(path))
-            new_child.parent = self
-            new_child.pave_way(cdr(path))
+            if car(path) not in self:
+                next_mole = Mole()  # a dummy molecule
+                self[car(path)] = next_mole 
+            else:
+                next_mole = self[car(path)]
+            next_mole.pave_way(cdr(path))
 
-    def kattach(self, node: Union[Atom, 'Mole'],
-                path: str = '', mode: str = 'merge', role: str=None):
+    def merge(self, val, path: str):
         """
-        Attach `node` to this molecule, with optional path down the line
-        :param role: the new role that `node` node will take
+        Merge/attach `value` to this molecule, with optional path down the line
         """
-        if role is not None:
-            node.role = role
-        if path != '':
-            self.pave_way(path)
-            self.get_path(path).kattach(node, mode=mode)
-        elif mode == 'merge':
-            if type(node) == Atom:
-                if self.has_path(node.role):
-                    same = self.get_path(node.role)
-                    same.vals = same.vals & node.vals
-                    same.legit = node.legit
-                else: node.parent = self
-            elif type(node) == Mole:
-                if self.has_path(node.role):
-                    same = self.get_path(node.role)
-                    if same.type == None:
-                        same.type = node.type
-                    else:
-                        assert(same.type == node.type), 'Two type, one node?'
-                    same.cons = same.cons & node.cons
-                    same.legit = node.legit
-                    for node_child in node.children:
-                        same.kattach(node_child, mode=mode)
-                else: node.parent = self
-        elif mode == 'overwrite':
-            if self.has_path(node.role):
-                same = self.get_path(node.role)
-                same.parent = None
-            node.parent = self
+        assert(path != ''), 'Empty path doesn\'t make sense'
+        # When the path has many levels down
+        if rcdr(path) != '':
+            self.pave_way(rcdr(path))
+            self.get_path(rcdr(path)).attach(val=val, path=rcar(path), mode=mode)
+        # When the path goes down only one level
+        elif path not in self:
+            self[path] = val
+        else:
+            if type(val) != Mole:  # `val` is a KSet
+                self[path] = self[path] & val
+            else:  # `val` is a molecule, do recursive merge
+                for key in val:
+                    self[path].merge(val=val[key], path=key, mode=mode)
 
     def clone(self) -> 'Mole':
-        res = Mole(role=self.role, type_=self.type, cons=self.cons.clone())
-        # Clone all the children, too
-        for child in self.children:
-            child.clone().parent = res
-        res.legit = self.legit
+        res = Mole()
+        for key in self:
+            # `self[key]` is either `KSet` or `Mole`
+            # which has `clone()`
+            clone[key] = self[key].clone()
         return res
 
     def __eq__(self, other) -> bool:
         res = True
-        if type(self) == type(other) and len(self.children) == len(other.children):
-            for child in self.children:
-                if (not other.has_path(child.role)) or (child != other.get_path(child.role)):
+        if len(self) == len(other):
+            for key in self:
+                if (key not in other) or (self[key] != other[key]):
                     res = False; break
-        else: res = False
+        else:
+            res = False
         return res
 
     def __hash__(self) -> int:
         """Hopefully this does not take too much time"""
-        return hash((self.type, self.cons, self.children))
-
-ATMO = Union[Atom, Mole]
+        return hash(tuple(self.items()))
